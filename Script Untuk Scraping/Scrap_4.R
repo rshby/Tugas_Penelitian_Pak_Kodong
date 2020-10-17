4c67c78095bc6fd7ebb1f2d418e63ed046a0344e
Reo Sahobby <reoshby@gmail.com>
2020-10-17 09:58:30 +0700

    Update Merapikan Scraping

--- Script Untuk Scraping/Scrap_4.R
+++ Script Untuk Scraping/Scrap_4.R
@@ -8,18 +8,15 @@ token <- create_token(
   access_token = "538784946-jVMIlJtz1wKRk9wpAjQMRCwqy7VfEDuYoS5tezMk",
   access_secret = "iRkYrIuaHZXOLPZQHPB2LXyaRVTXSvqyVWd29l567Gdb9")
 
-first_account <- list("kbridenhaag")
-users <- list("IndonesiaInCPH", "KBRI_Athena", "kbrimoskow", "kbrihelsinki",
-              "KBRI_Paris", "IndonesiaInSto", "IndonesiaInPL", "KBRIPraha", "KBRIBerlin", "IndonesiainBern",
-              "kbri_baku", "KBRI__Roma", "kbriwina", "kbrioslo", "KBRILondon", "KBRIMADRID",
-              "INAinLisbon", "indonesia_sk", "KBRI_Beograd", "kbribxl", "KBRISarajevo", "kbrisofia", "indonesiazagreb",
-              "INAinBucharest", "KBRIWashDC", "KBRI_Brasilia", "kbrisantiago", "KBRIMexicoCity",
-              "KBRI_Beijing", "IdEmbassy_Seoul", "INAinPyongyang", "IndonesiaEmbVte", 
-              "IndonesiaInBSB", "kbridili", "kbriportmoresby", "kbriphnompenh", "KBRI_NewDelhi", 
-              "IndonesiaHanoi", "IndonesiaAnkara", "KBRI_Ottawa", "KBRIPanamaCity", "KbriHavana", 
-              "KBRIBuenosAires", "kbri_caracas", "kbrilima", "IndonesiainCOL", "KbriParamaribo", "kbri_kyiv",
-              "kbri_tashkent", "KbriYangon", "KBRI_Islamabad", "KBRITokyo", "IndonesiaDhaka", "KBRI_Manila",
-              "kbrikualalumpur", "kbrisingapura", "KBRI_Bangkok")
+first_account <- list("IndonesiaHanoi")
+users <- list('KBRITokyo', 'IndonesiaInPL', 'KBRI_NewDelhi', 'kbrikualalumpur', 'kbrilima',
+              'kbrimoskow', 'KBRI_Bangkok', 'KBRI_Ottawa', 'KBRI_Islamabad', 'KBRIMexicoCity', 'KBRIWashDC', 'kbribxl',
+              'IndonesiaInCPH', 'KBRI_Beijing', 'kbriwina', 'KBRI_Beograd', 'KBRI_Athena', 'kbrisantiago',
+              'indonesia_sk', 'INAinBucharest', 'KBRI_Paris', 'kbrioslo', 'KBRI__Roma', 'IndonesiaAnkara', 'kbrihelsinki',
+              'IndonesiaInBSB', 'KBRI_Brasilia', 'kbriphnompenh', 'IndonesiaInSto', 'kbridili', 'kbriportmoresby',
+              'IndonesiainBern', 'KBRILondon', 'KBRIPraha', 'KBRIBuenosAires', 'IndonesiaDhaka', 'KBRIMADRID',
+              'kbri_kyiv', 'KBRIPanamaCity', 'kbri_baku', 'INAinPyongyang', 'KBRI_Manila', 'KBRISarajevo', 'kbri_tashkent',
+              'kbrisofia', 'IdEmbassy_Seoul', 'kbridenhaag', 'IndonesiainCOL', 'kbrisingapura')
 
 for (awal in first_account){
   tweets <- get_timelines(awal, n=5)
