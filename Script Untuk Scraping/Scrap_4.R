fbc3e8d79438db0efb5c8130146b6c05803d9f29
Reo Sahobby <reoshby@gmail.com>
2020-10-17 10:23:04 +0700

    Add new File

--- Script Untuk Scraping/Scrap_4.R
+++ Script Untuk Scraping/Scrap_4.R
@@ -19,28 +19,14 @@ users <- list('KBRITokyo', 'IndonesiaInPL', 'KBRI_NewDelhi', 'kbrikualalumpur', 
               'kbrisofia', 'IdEmbassy_Seoul', 'kbridenhaag', 'IndonesiainCOL', 'kbrisingapura')
 
 for (awal in first_account){
-  tweets <- get_timelines(awal, n=5)
+  tweets <- get_timelines(awal, n=3200)
   df = data.frame(lapply(tweets, as.character))
   for (account in users){
-    hasil_tweet <- get_timelines(account, n=5)
+    hasil_tweet <- get_timelines(account, n=3200)
     df_new <- data.frame(lapply(hasil_tweet, as.character))
     df <- rbind(df, df_new)
   }
   write.csv(df, file = "hasil_scrap_4.csv")
 }
-
-for (akun in users) {
-  tweets <- get_timelines(akun, n=3200)
-  df = data.frame(lapply(tweets, as.character))
-}
-write.csv(df, file = sprintf("%s.csv", akun))
-
-# -------- List Akun yang ingin di scrap ----------
-users <- list("kbridenhaag", "IndonesiaInCPH", "KBRI_Athena", "kbrimoskow", "kbrihelsinki",
-              "KBRI_Paris", "IndonesiaInSto", "IndonesiaInPL", "KBRIPraha", "KBRIBerlin", "IndonesiainBern",
-              "kbri_baku", "KBRI__Roma", "kbriwina", "kbrioslo", "KBRILondon", "KBRIMADRID",
-              "INAinLisbon", "indonesia_sk", "KBRI_Beograd", "kbribxl", "KBRISarajevo", "kbrisofia", "indonesiazagreb",
-              "INAinBucharest", "KBRIWashDC", "KBRI_Brasilia", "kbrisantiago", "KBRIMexicoCity")
-
 
 
